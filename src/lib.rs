d434e2ed562d033d02f0112b28d2dbd5f433cc83
Kana Otawara <shimmer22.0204@gmail.com>
2022-03-23 02:18:19 +0900

    call search from run

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,10 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents: String = fs::read_to_string(config.filename)?;
 
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
     println!("With text:\n{}", contents);
 
     Ok(())
