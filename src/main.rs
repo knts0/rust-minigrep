4a046a593b92115d0cc452e0f3f2d63d291ba46e
Kana Otawara <shimmer22.0204@gmail.com>
2022-03-23 01:42:57 +0900

    extract logic to run fn

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,16 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let contents: String = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
 }
+
 struct Config {
     query: String,
     filename: String,
